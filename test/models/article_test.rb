e03ab65fc0c13d2d27338e3b96e893d25e8bc003
m3thom <39703347+m3thom@users.noreply.github.com>
2023-07-21 12:04:02 +0700

    Add simple `article` scaffold

---
+++ test/models/article_test.rb
@@ -0,0 +1,133 @@
+require "test_helper"
+
+class ArticleTest < ActiveSupport::TestCase
+  test "should create valid article with title" do
+    assert Article.create(title: "Test title")
+  end
+
+  test "should create valid article with content" do
+    assert Article.create(content: "Test title")
+  end
+
+  # Dummies for parallelize test
+  test "dummy 1" do
+    assert Article.create(content: "Test title 1")
+  end
+  test "dummy 2" do
+    assert Article.create(content: "Test title 2")
+  end
+  test "dummy 3" do
+    assert Article.create(content: "Test title 3")
+  end
+  test "dummy 4" do
+    assert Article.create(content: "Test title 4")
+  end
+  test "dummy 5" do
+    assert Article.create(content: "Test title 5")
+  end
+  test "dummy 6" do
+    assert Article.create(content: "Test title 6")
+  end
+  test "dummy 7" do
+    assert Article.create(content: "Test title 7")
+  end
+  test "dummy 8" do
+    assert Article.create(content: "Test title 8")
+  end
+  test "dummy 9" do
+    assert Article.create(content: "Test title 9")
+  end
+  test "dummy 10" do
+    assert Article.create(content: "Test title 10")
+  end
+  test "dummy 11" do
+    assert Article.create(content: "Test title 11")
+  end
+  test "dummy 12" do
+    assert Article.create(content: "Test title 12")
+  end
+  test "dummy 13" do
+    assert Article.create(content: "Test title 13")
+  end
+  test "dummy 14" do
+    assert Article.create(content: "Test title 14")
+  end
+  test "dummy 15" do
+    assert Article.create(content: "Test title 15")
+  end
+  test "dummy 16" do
+    assert Article.create(content: "Test title 16")
+  end
+  test "dummy 17" do
+    assert Article.create(content: "Test title 17")
+  end
+  test "dummy 18" do
+    assert Article.create(content: "Test title 18")
+  end
+  test "dummy 19" do
+    assert Article.create(content: "Test title 19")
+  end
+  test "dummy 20" do
+    assert Article.create(content: "Test title 20")
+  end
+  test "dummy 21" do
+    assert Article.create(content: "Test title 21")
+  end
+  test "dummy 22" do
+    assert Article.create(content: "Test title 22")
+  end
+  test "dummy 23" do
+    assert Article.create(content: "Test title 23")
+  end
+  test "dummy 24" do
+    assert Article.create(content: "Test title 24")
+  end
+  test "dummy 25" do
+    assert Article.create(content: "Test title 25")
+  end
+  test "dummy 26" do
+    assert Article.create(content: "Test title 26")
+  end
+  test "dummy 27" do
+    assert Article.create(content: "Test title 27")
+  end
+  test "dummy 28" do
+    assert Article.create(content: "Test title 28")
+  end
+  test "dummy 29" do
+    assert Article.create(content: "Test title 29")
+  end
+  test "dummy 30" do
+    assert Article.create(content: "Test title 30")
+  end
+  test "dummy 31" do
+    assert Article.create(content: "Test title 31")
+  end
+  test "dummy 32" do
+    assert Article.create(content: "Test title 32")
+  end
+  test "dummy 33" do
+    assert Article.create(content: "Test title 33")
+  end
+  test "dummy 34" do
+    assert Article.create(content: "Test title 34")
+  end
+  test "dummy 35" do
+    assert Article.create(content: "Test title 35")
+  end
+  test "dummy 36" do
+    assert Article.create(content: "Test title 36")
+  end
+  test "dummy 37" do
+    assert Article.create(content: "Test title 37")
+  end
+  test "dummy 38" do
+    assert Article.create(content: "Test title 38")
+  end
+  test "dummy 39" do
+    assert Article.create(content: "Test title 39")
+  end
+  test "dummy 40" do
+    assert Article.create(content: "Test title 40")
+  end
+end
